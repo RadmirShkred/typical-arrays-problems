5d08b32d373094e9e16682558806104a93bd9e3b
RadmirShkred <psitecton@gmail.com>
2021-08-14 22:09:16 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0) return 0;
+  const result = array.sort((a, b) => { return a - b });
+  return result[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0) return 0;
+  const result = array.sort((a, b) => { return a - b });
+  return result[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length == 0) return 0;
+  const result = array.reduce((a, b) => { return a + b });
+  return result / array.length;
 }
